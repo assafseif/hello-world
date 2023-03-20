23d8ceb6069077c6a40281f5fbde44f5bd5c4481
assafseif <75131252+assafseif@users.noreply.github.com>
2023-03-20 12:04:41 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello  omar nasrallah 345')
+  res.send('Hello  charbel chahine')
 })
 
 app.listen(port, () => {
